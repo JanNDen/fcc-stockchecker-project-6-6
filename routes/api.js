d130aa175fa5442971cf7f14793a746e9a22fac3
JanNDen <jc@pntr.eu>
2021-03-01 13:51:21 +0200

    Clean slate

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,17 @@
 'use strict';
 
+// Connection
+const mongoose = require("mongoose");
+const db = mongoose.connect(process.env.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const { Schema } = mongoose;
+
+// Model
+const stockSchema = new Schema({});
+const StockModel = mongoose.model("stock", stockSchema);
+
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
